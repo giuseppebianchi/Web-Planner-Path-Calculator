c4da38403cb8bafcc4726923b0eb8fad10b4a94d
Giuseppe Bianchi <gppbianchi@gmail.com>
2017-01-28 15:30:27 +0100

    Bug Fix -  Generation Values of Attributes

--- routes/create.js
+++ routes/create.js
@@ -23,18 +23,21 @@ router.use(bodyParser.urlencoded({"extended" : false}));
 
 //This function generates floating-point between two numbers low (inclusive) and high (exclusive) ([low, high))
 var random = function(low, high) {
-    return Math.random() * (high - low) + low;
+	var number = Math.random() * (high - low) + low;
+	console.log(number, low, high)
+    return parseFloat(number).toFixed(2);
 };
 
 //This function generates random integer between two numbers low (inclusive) and high (exclusive) ([low, high))
 var randomInt = function(low, high) {
-    return Math.floor(Math.random() * (high - low) + low);
+	var number = Math.floor(Math.random() * (high - low) + low)
+	console.log(number, low, high)
+    return number;
 };
 
 //GENERATE ATTRIBUTES VALUES FUNCTION
 function setAttributesValues(list){
 	//list is the array of attributes
-
 	var attributesValues = [];
 
 	for (var i = 0, len = list.length; i < len; i++) {
@@ -42,7 +45,7 @@ function setAttributesValues(list){
 		if(list[i].isInteger == 1){
 			obj.value = randomInt(list[i].k, list[i].n);
 		}else{
-			obj.value = randomInt(list[i].k, list[i].n);
+			obj.value = random(list[i].k, list[i].n);
 		}
 		obj.name = list[i].name
 		attributesValues.push(obj)
@@ -106,7 +109,6 @@ var buildTree = function buildTreeRecursive(key, albero, split, depth, ant, k){
 }
 
 router.post("/",function(req,res){
-	console.log(req.body)
 	console.time("perf")
 	anchestors = [];
 	index = 0;
@@ -124,11 +126,15 @@ router.post("/",function(req,res){
 	var vertecesAttributeList = [], edgesAttributeList = [];
 	var isIntegerV = JSON.parse(req.body['isIntegerV[]']);
 	var isIntegerE = JSON.parse(req.body['isIntegerE[]']);
-
 	//ATTRIBUTES FOR NODES
 	if( typeof req.body['vertexAttrName[]'] === 'string' ) {
-		console.log("qui")
     	req.body['vertexAttrName[]'] = [ req.body['vertexAttrName[]'] ];
+	}
+	if( typeof req.body['nValueVertex[]'] === 'string' ) {
+    	req.body['nValueVertex[]'] = [ req.body['nValueVertex[]'] ];
+	}
+	if( typeof req.body['kValueVertex[]'] === 'string' ) {
+    	req.body['kValueVertex[]'] = [ req.body['kValueVertex[]'] ];
 	}
 	for (var i = 0, len = req.body['vertexAttrName[]'].length; i < len; i++) {
 		var temp = {};
@@ -145,25 +151,28 @@ router.post("/",function(req,res){
 
 	//ATTRIBUTES FOR EDGES
 	if( typeof req.body['edgeAttrName[]'] === 'string' ) {
-		console.log("qua")
     	req.body['edgeAttrName[]'] = [ req.body['edgeAttrName[]'] ];
+	}
+	if( typeof req.body['kValueEdge[]'] === 'string' ) {
+    	req.body['kValueEdge[]'] = [ req.body['kValueEdge[]'] ];
+	}
+	if( typeof req.body['nValueEdge[]'] === 'string' ) {
+    	req.body['nValueEdge[]'] = [ req.body['nValueEdge[]'] ];
 	}
 	for (var i = 0, len = req.body['edgeAttrName[]'].length; i < len; i++) {
 		var temp = {};
 		temp.name = req.body['edgeAttrName[]'][i];
-	  	temp.ref = "Node";
+	  	temp.ref = "Edge";
 	  	//per il momento la funzione di generazione valori è statica
 	  	//temp.ref = req.body.vertexGenerationRule[i];
-	  	temp.k = req.body['kValueEdge[]'][i];
-	  	temp.n = req.body['nValueEdge[]'][i];
+	  	temp.k = parseFloat(req.body['kValueEdge[]'][i]);
+	  	temp.n = parseFloat(req.body['nValueEdge[]'][i]);
 	  	temp.isInteger = isIntegerE[i];
 	  	edgesAttributeList.push(temp);
 
 	}
-
 	t.vertecesAttributeList = vertecesAttributeList;
 	t.edgesAttributeList = edgesAttributeList;
-
 	//INSERT TREE
 	Tree.createTree(t, function(err, data){
 		if(err){
@@ -172,11 +181,11 @@ router.post("/",function(req,res){
 		t.id = data._id;
 		//CREATE NODES
 		buildTree(0, t, t.splitSize, t.depthSize, [], 0);
+		res.json(t);
 		//res.json("ok");
 	})
 
 
-    res.json(t);
     //res.json("ok");
 });
 
